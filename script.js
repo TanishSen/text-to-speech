cdc9ce16520d0530d6ad6b5c39bc70e5555f87f1
TanishSen <tanishsen.2520@gmail.com>
2024-12-01 20:02:17 +0530

    1. Changes in the UI

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+// let speech = new SpeechSynthesisUtterance();
+
+// let voices = [];
+
+// let voiceSelect = document.querySelector("select");
+
+// window.speechSynthesis.onvoiceschanged = () => {
+//   voices = window.speechSynthesis.getVoices;
+//   speech.voice = voices[0];
+
+//   voices.forEach(
+//     (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+//   );
+// };
+
+// document.querySelector("button").addEventListener("click", () => {
+//   speech.text = document.querySelector("textarea").value;
+//   window.speechSynthesis.speak(speech);
+
+//   voiceSelect.addEventListener("change", () => {
+//     speech.voice = voices[voiceSelect.value];
+//   });
+// });
+
 let speech = new SpeechSynthesisUtterance();
 
 let voices = [];
-
 let voiceSelect = document.querySelector("select");
 
+// Populate the voices when they change
 window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices;
-  speech.voice = voices[0];
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0]; // Set default voice
 
-  voices.forEach(
-    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
-  );
+  // Populate the dropdown with voices
+  voiceSelect.innerHTML = ""; // Clear existing options
+  voices.forEach((voice, i) => {
+    voiceSelect.options[i] = new Option(voice.name, i);
+  });
 };
 
+// Event listener for voice selection
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+// Speak the text when the button is clicked
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
-
-  voiceSelect.addEventListener("change", () => {
-    speech.voice = voices[voiceSelect.value];
-  });
 });
